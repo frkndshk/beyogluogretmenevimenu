18122a535135c5125993b2150018d404bae842e5
frkndshk <frknsdhk@gmail.com>
2024-02-24 03:12:14 +0300

    Update dependencies

--- server.js
+++ server.js
@@ -3,17 +3,27 @@ const mysql = require('mysql2');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 3002; // Heroku tarafýndan saðlanan PORT veya yerelde 3002'yi kullan
+const port = process.env.PORT || 3002;
 
 const connection = mysql.createConnection({
-    host: 'brflrxzq5tg82kflfbmv-mysql.services.clever-cloud.com',
-    user: 'uqcw9bdlanbwc4sn',
-    password: 'CP8ER0OfZj4xD01po2xJ',
-    database: 'brflrxzq5tg82kflfbmv'
+    host: process.env.DB_HOST || 'brflrxzq5tg82kflfbmv-mysql.services.clever-cloud.com',
+    user: process.env.DB_USER || 'uqcw9bdlanbwc4sn',
+    password: process.env.DB_PASSWORD || 'CP8ER0OfZj4xD01po2xJ',
+    database: process.env.DB_DATABASE || 'brflrxzq5tg82kflfbmv'
 });
 
 // CORS middleware
 app.use(cors());
+
+// Veritabaný baðlantý olaylarýný izleme
+connection.connect(function (err) {
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+        return;
+    }
+
+    console.log('Connected to MySQL');
+});
 
 app.get('/api/menuler', (req, res) => {
     connection.query('SELECT * FROM menuler', (error, results, fields) => {
@@ -27,7 +37,6 @@ app.get('/api/menuler', (req, res) => {
     });
 });
 
-// Heroku'da çalýþtýðýnýzda, aþaðýdaki kodu kullanarak dinamik olarak bir port seçin
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
